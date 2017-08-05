47d2c2fc5ecfeb6bb7664691e0bafb240593ee6c
jonvtruong <jonvtruong@gmail.com>
2017-08-04 21:56:32 -0400

    switched to astral, can now turn off bulb and turn on lamp after sunset

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
-'''
-Sunset API: https://sunrise-sunset.org
-'''
-
 from pyHS100 import SmartBulb, SmartPlug
 from datetime import datetime, timedelta
-
-import requests
-
-googleParams = {'address': '02134', 'key': 'AIzaSyBqnPBbHIGdTwE-QVRtbwXUVPnzisQ4Prc'}
+import pytz, time
+from astral import Astral
 
 plugIP = '192.168.1.103'
 bulbIP = '192.168.1.104'
 
-def sunset():
-    google = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=googleParams)
-   # print("Google status: " + str(google.status_code))
-    googleDict = google.json()
-    
-    #34x per minute
-    # for k,v in googleDict['results'].items():
-    #      print(str(k) + ": " + str(v))
+cityName = 'Boston'
+'''
+for list of possible cities: http://pythonhosted.org/astral/index.html
 
-    googleDict['results'][0]['geometry']['location']['formatted'] = 0
+'''
+# returns true if the current time is between dusk and dawn using the Astral library
 
+def sunset():    
+    a = Astral()
+    location = a[cityName]
+    location.solar_depression = 'civil' #defines angle of sun to calculate time
+    sun = location.sun(date=datetime.today(), local=True) #returns a dictionary containing sunset,sunrise,dusk,dawn : times
 
-    sun = requests.get('https://api.sunrise-sunset.org/json', params=googleDict['results'][0]['geometry']['location'])
-  #  print("sun status: " + str(sun.status_code))
-    sunDict = sun.json()
-   # sunsetTime = datetime.strptime(sunDict['results']['sunset'], '%I:%M:%S %p')
-    sunsetTime = sunDict['results']['sunset']
-    nowUTC = datetime.utcnow()
-    print("sunset: " + str(sunsetTime))
-    print("time now: " + str(nowUTC))
+    timeSunset = sun['dusk']
+    timeNow = datetime.now(pytz.timezone(location.timezone)) #gets current time with same time zone to be time zone aware
+    timeSunrise = sun['dawn'] + timedelta(days=1) #gets sunrise time of next day
 
-    d = timedelta(minutes = 30)
+    # print("sunset: " + str(timeSunset))
+    # print('sunrise: ' + str(timeSunrise))
 
-    #if(nowUTC < sunsetTime):
-    #    print("its sunset")
-
-    return False
+    return (timeNow < timeSunrise and timeNow > timeSunset)
 
 if (__name__ == "__main__"):
     plug = SmartPlug(plugIP)
     bulb = SmartBulb(bulbIP)
-    sunset()
 
-    # if(bulb.is_on):
-    #     plug.turn_on()
-    #     bulb.turn_off()
-
+    if(bulb.is_on and sunset()): #if the bulb is currently on and it's night time then turn off the bulb and turn on lamp#transition to 50% then turn on lamp then transition to 0
+        bulb.set_light_state({'on_off': 0, 'transition_period': 3000})
+        plug.turn_on()
+        
+'''
+    #for easier testing comment out for implementation
+    elif(plug.is_on): #if daytime and the lamp is still on, turn it off
+        bulb.set_light_state({'on_off': 1, 'transition_period': 3000})
+        plug.turn_off()
+'''
